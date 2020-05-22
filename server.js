d376a3ea6fa10608d0c5aeba5635fcc38d862440
DontKillMyMojo <dadzoh@hotmail.com>
2020-05-22 14:31:03 +0300

    testing multiple sockets to select one client

--- server.js
+++ server.js
@@ -41,9 +41,14 @@ app.get('/', (req, res) => {
     // tslint:disable-next-line: quotemark
     //uncomment res.send if what is above does not work
     //res.send(path.resolve('client/index.html'));
-    res.send("Serveur projet noé");
+    //res.send("Serveur projet noé");
+    
+    //redirection on web
+    res.writeHead(301,
+    {Location: 'https://sleepy-taiga-76984.herokuapp.com/'}
+    );
 });
-io.on("connection", function (socket) {
+io.sockets.on("connection", function (socket) {
     console.log("a user connected");
     // whenever we receive a 'message' we log it out
     socket.on("message", function (message) {
